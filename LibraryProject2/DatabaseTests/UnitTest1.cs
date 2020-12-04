c5c4e24919132efdf9edfbd450cfb2cb2f0ecc19
224085 Barbara Borkowska <224085@edu.p.lodz.pl>
2020-12-04 15:22:57 +0100

    DatabaseTests.

---
+++ LibraryProject2/DatabaseTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace DatabaseTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void AddBookToDatabase()
+        {
+            
+            Assert.Pass();
+        }
+    }
+}
