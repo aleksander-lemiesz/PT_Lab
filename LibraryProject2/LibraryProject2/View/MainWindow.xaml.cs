06e09a299caaaadb9442cfeededa0e1d2c16f3a5
224085 Barbara Borkowska <224085@edu.p.lodz.pl>
2020-12-05 15:21:10 +0100

    Tabs added to MainWindow.

--- LibraryProject2/LibraryProject2/View/MainWindow.xaml.cs
+++ LibraryProject2/LibraryProject2/View/MainWindow.xaml.cs
@@ -27,17 +27,5 @@ namespace LibraryProject2
         }
 
 
-      /*  private void LoadButton_Click(object sender, RoutedEventArgs e)
-        {
-            LoadCustomers();
-        }
-        private void LoadCustomers()
-        {
-            LinqToSqlLib.LibraryDataContext cd = new LinqToSqlLib.LibraryDataContext();
-            var customers = (from p in cd.Customers
-                             select p).Take(1);
-            MyDataGrid.ItemsSource = customers;
-            LoadButton.Content = "Customers Loaded";
-        }*/
     }
 }
